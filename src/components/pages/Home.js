d134aabaabacfcca3e5571560692061586914808
RaphaelAlcantara <rvfra@discente.ifpe.edu.br>
2022-12-20 20:02:40 -0300

    Home finalizada e criação de componente botão

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,9 +1,15 @@
+import styles from './Home.module.css'
+import savings from '../../img/savings.svg'
+import LinkButton from "../layout/LinkButton";
+
 function Home () {
     return (
-        <div>
-            <h1>Home</h1>
-            <p>Home page</p>
-        </div>
+        <section className={styles.home_container}>
+            <h1>Bem-vindo ao <span>Costs</span></h1>
+            <p>Gerencie seus gastos de forma simples e eficiente</p>
+            <LinkButton to="/new-project" text="Criar novo projeto"/>
+            <img src={savings} alt="Costs" />
+        </section>
     )
 }
 
