e4f7d1f0765591ea81b32ad4ec4a2f81d623f8d8
RaphaelAlcantara <rvfra@discente.ifpe.edu.br>
2022-12-20 16:19:31 -0300

    Navbar Finalizada

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import Home from "./components/pages/Home";
+import Company from "./components/pages/Company";
+import Contact from "./components/pages/Contact";
+import NewProject from "./components/pages/NewProject";
 
+import Container from "./components/layout/Container";
+import Navbar from "./components/layout/Navbar";
+import {Footer} from "./components/layout/Footer";
+import Projects from "./components/pages/Projects";
 
 function App() {
   return (
-    <div className="App">
-     <p>Costs</p>
-    </div>
+    <Router>
+        <Navbar />
+        <Switch>
+            <Container customClass = "min-height">
+            <Route exact path="/">
+                <Home />
+            </Route>
+            <Route path="/projects">
+                <Projects/>
+            </Route>
+            <Route path="/company">
+                <Company />
+            </Route>
+            <Route path="/contact">
+                <Contact/>
+            </Route>
+            <Route path="/new-project">
+                <NewProject/>
+            </Route>
+            </Container>
+        </Switch>
+        <Footer/>
+    </Router>
   );
 }
 
---
+++ src/components/pages/NewProject.js
@@ -0,0 +1,10 @@
+function NewProject () {
+    return (
+        <div>
+            <h1>NewProject</h1>
+            <p>NewProject page</p>
+        </div>
+    )
+}
+
+export default NewProject;
